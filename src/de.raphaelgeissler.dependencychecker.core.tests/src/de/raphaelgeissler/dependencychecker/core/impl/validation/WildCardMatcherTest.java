1928dc998a9d4b1af763ac32c6258412c4295763
Raphael Geissler <raphael.geissler@gmail.com>
2016-03-15 14:23:27 +0100

    Made access to WildCardMatcher static.

--- src/de.raphaelgeissler.dependencychecker.core.tests/src/de/raphaelgeissler/dependencychecker/core/impl/validation/WildCardMatcherTest.java
+++ src/de.raphaelgeissler.dependencychecker.core.tests/src/de/raphaelgeissler/dependencychecker/core/impl/validation/WildCardMatcherTest.java
@@ -8,36 +8,36 @@ public class WildCardMatcherTest {
 
 	@Test
 	public void wildCardAtEndMatchingTest() throws Exception {
-		assertTrue(new WildCardMatcher().isMatching("de.ragedev.core.plugin1","de.ragedev.core.*"));
+		assertTrue(WildCardMatcher.isMatching("de.ragedev.core.plugin1","de.ragedev.core.*"));
 	}
 	
 	@Test
 	public void wildCardAtEndNotMatchingTest() throws Exception {
-		assertFalse(new WildCardMatcher().isMatching("de.ragedev.core.plugin1","de.ragedev.ui.*"));
+		assertFalse(WildCardMatcher.isMatching("de.ragedev.core.plugin1","de.ragedev.ui.*"));
 	}
 	
 	@Test
 	public void wildCardAtMidMatchingTest() throws Exception {
-		assertTrue(new WildCardMatcher().isMatching("de.ragedev.core.plugin1","de.ragedev.*.plugin1"));
+		assertTrue(WildCardMatcher.isMatching("de.ragedev.core.plugin1","de.ragedev.*.plugin1"));
 	}
 	
 	@Test
 	public void wildCardAtMidNotMatchingTest() throws Exception {
-		assertFalse(new WildCardMatcher().isMatching("de.ragedev.core.plugin1","de.ragedev.*.plugin2"));
+		assertFalse(WildCardMatcher.isMatching("de.ragedev.core.plugin1","de.ragedev.*.plugin2"));
 	}
 	
 	@Test
 	public void wildCardAtStartMatchingTest() throws Exception {
-		assertTrue(new WildCardMatcher().isMatching("de.ragedev.core.plugin1","*.plugin1"));
+		assertTrue(WildCardMatcher.isMatching("de.ragedev.core.plugin1","*.plugin1"));
 	}
 	
 	@Test
 	public void wildCardAtStartNotMatchingTest() throws Exception {
-		assertFalse(new WildCardMatcher().isMatching("de.ragedev.core.plugin1","*.plugin2"));
+		assertFalse(WildCardMatcher.isMatching("de.ragedev.core.plugin1","*.plugin2"));
 	}
 	
 	@Test
 	public void wildCardExactlyMatchingTest() throws Exception {
-		assertFalse(new WildCardMatcher().isMatching("de.ragedev.ui","de.ragedev.ui.*"));
+		assertFalse(WildCardMatcher.isMatching("de.ragedev.ui","de.ragedev.ui.*"));
 	}
 }
