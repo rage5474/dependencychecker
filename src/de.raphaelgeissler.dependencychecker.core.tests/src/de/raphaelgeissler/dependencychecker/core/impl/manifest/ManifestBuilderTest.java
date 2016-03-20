ecad08ccb1e1d8b2798fa515fad72d4ec40acf97
Raphael Geissler <raphael.geissler@gmail.com>
2016-03-20 09:28:21 +0100

    Added importPackage in ManifestBuilder to be able to write simple tests.

--- src/de.raphaelgeissler.dependencychecker.core.tests/src/de/raphaelgeissler/dependencychecker/core/impl/manifest/ManifestBuilderTest.java
+++ src/de.raphaelgeissler.dependencychecker.core.tests/src/de/raphaelgeissler/dependencychecker/core/impl/manifest/ManifestBuilderTest.java
@@ -12,12 +12,13 @@ public class ManifestBuilderTest {
 
 	@Test
 	public void simpleTest() throws Exception {
-		Manifest m = new ManifestBuilder().symbolicName("MyBundleID").addRequiredBundle("MyDepPlugin1").addRequiredBundle("MyDepPlugin", "1.0.0").build();
+		Manifest m = new ManifestBuilder().symbolicName("MyBundleID").addRequiredBundle("MyDepPlugin1").addImportPackage("MyPackage1").addRequiredBundle("MyDepPlugin", "1.0.0").addImportPackage("MyPackage2","1.0.0").build();
 		
 		assertEquals("1.0" , m.getMainAttributes().getValue("Manifest-Version"));
 		assertEquals("2" , m.getMainAttributes().getValue("Bundle-ManifestVersion"));
 		assertEquals("MyBundleID" , m.getMainAttributes().getValue("Bundle-SymbolicName"));
 		assertEquals("MyDepPlugin1,MyDepPlugin;bundle-version=\"1.0.0\"" , m.getMainAttributes().getValue("Require-Bundle"));
+		assertEquals("MyPackage1,MyPackage2;version=\"1.0.0\"" , m.getMainAttributes().getValue("Import-Package"));
 	}
 	
 }
--- src/de.raphaelgeissler.dependencychecker.core/src/de/raphaelgeissler/dependencychecker/core/impl/manifest/ManifestBuilder.java
+++ src/de.raphaelgeissler.dependencychecker.core/src/de/raphaelgeissler/dependencychecker/core/impl/manifest/ManifestBuilder.java
@@ -6,14 +6,15 @@ import java.util.jar.Manifest;
 public class ManifestBuilder {
 
 	private Manifest manifest;
-private String requiredBundleIds = "";
-	
+	private String requiredBundleIds = "";
+	private String importPackageIds = "";
+
 	public ManifestBuilder() {
 		this.manifest = new Manifest();
 		this.manifest.getMainAttributes().putValue(Name.MANIFEST_VERSION.toString(), "1.0");
 		this.manifest.getMainAttributes().putValue("Bundle-ManifestVersion", "2");
 	}
-	
+
 	public ManifestBuilder symbolicName(String symbolicName) {
 		manifest.getMainAttributes().putValue("Bundle-SymbolicName", symbolicName);
 		return this;
@@ -23,19 +24,30 @@ private String requiredBundleIds = "";
 		requiredBundleIds += "," + requiredBundleID;
 		return this;
 	}
-	
+
 	public ManifestBuilder addRequiredBundle(String requiredBundleID, String bundleVersion) {
 		requiredBundleIds += "," + requiredBundleID + ";bundle-version=\"" + bundleVersion + "\"";
 		return this;
 	}
 
 	public Manifest build() {
-		if(!requiredBundleIds.isEmpty())
-		{
+		if (!requiredBundleIds.isEmpty()) {
 			manifest.getMainAttributes().putValue("Require-Bundle", requiredBundleIds.substring(1));
+		}
+		if (!importPackageIds.isEmpty()) {
+			manifest.getMainAttributes().putValue("Import-Package", importPackageIds.substring(1));
 		}
 		return manifest;
 	}
 
-	
+	public ManifestBuilder addImportPackage(String packageName) {
+		importPackageIds += "," + packageName;
+		return this;
+	}
+
+	public ManifestBuilder addImportPackage(String packageName, String version) {
+		importPackageIds += "," + packageName + ";version=\"" + version + "\"";
+		return this;
+	}
+
 }
