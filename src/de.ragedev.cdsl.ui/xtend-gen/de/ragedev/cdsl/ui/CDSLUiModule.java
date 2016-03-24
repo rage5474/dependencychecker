b955e986ef15b305ea44a3d3d9ed3f77e3b03193
Raphael Geissler <raphael.geissler@gmail.com>
2016-03-24 10:01:33 +0100

    Switching to default plant-uml update-site.

--- src/de.ragedev.cdsl.ui/xtend-gen/de/ragedev/cdsl/ui/CDSLUiModule.java
+++ src/de.ragedev.cdsl.ui/xtend-gen/de/ragedev/cdsl/ui/CDSLUiModule.java
@@ -13,7 +13,7 @@ import org.eclipse.xtend.lib.annotations.FinalFieldsConstructor;
 @FinalFieldsConstructor
 @SuppressWarnings("all")
 public class CDSLUiModule extends AbstractCDSLUiModule {
-  public CDSLUiModule(final AbstractUIPlugin arg0) {
-    super(arg0);
+  public CDSLUiModule(final AbstractUIPlugin plugin) {
+    super(plugin);
   }
 }
